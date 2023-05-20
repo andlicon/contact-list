9c0eb6e06493908d38d8ecb4304b399ca6e8977c
andlicon <andresjzabalac@hotmail.com>
2023-05-20 14:48:11 -0400

    ADD: item was create

--- src/js/component/Item.jsx
+++ src/js/component/Item.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import '../../styles/item.css';
+
 const Item = ({ fullName,
   address,
   email,
@@ -9,14 +11,14 @@ const Item = ({ fullName,
     <div className='item'>
       <img className='item__image' src="" alt="" />
       <div className="item__content">
-        <h2 clasName='item__titulo'>{fullName}</h2>
+        <h2 className='item__titulo'>{fullName}</h2>
         <p className="item__addres">{address}</p>
         <p className="item__phone">{phone}</p>
         <p className="item__email">{email}</p>
       </div>
       <div className="item__interact">
-        <button clasName='item__button'><i className="bi bi-trash3"></i></button>
-        <button clasName='item__button'><i className="bi bi-pencil-fill"></i></button>
+        <button className='item__button'><i className="bi bi-trash3 delete"></i></button>
+        <button className='item__button'><i className="bi bi-pencil-fill edit"></i></button>
       </div>
 
     </div>
--- src/js/views/Contact.jsx
+++ src/js/views/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/contact.css';
 import { Context } from '../store/appContext';
